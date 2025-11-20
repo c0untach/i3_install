813882b6090120a07c01f29d4f7642fa8923bf3e
c0untach <51246376+c0untach@users.noreply.github.com>
2025-11-20 22:58:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 
 #TODO
 apt update -y
-apt install git curl wget i3 kitty xorg firefox-esr bumblebee-status sudo -y
+apt install git curl wget i3 kitty xorg firefox-esr bumblebee-status sudo pulseaudio pavucontrol tmux unzip -y
 git clone https://github.com/c0untach/dotfiles.git
