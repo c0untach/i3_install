9e26794d152efd967425384115d0639d4d44b027
c0untach <51246376+c0untach@users.noreply.github.com>
2025-11-26 12:20:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
-
 #TODO
 apt update -y
-apt install git curl wget i3 kitty xorg firefox-esr bumblebee-status sudo pulseaudio pavucontrol tmux unzip net-tools -y
-git clone https://github.com/c0untach/dotfiles.git
+apt install git ssh openssh-client curl wget i3 kitty xorg firefox-esr bumblebee-status sudo pulseaudio pavucontrol tmux unzip net-tools -y
+git clone https://github.com/c0untach/dotfiles.git ~/home/user/repos
